076dd8277bfd709b3cf8b3d0713fb606e3f7dcae
yuetsin <akaza_akari@sjtu.edu.cn>
2020-07-03 11:25:29 +0800

    [add] Delaunay 2D version gen plugins

--- Assets/Algorithm/VolMeshGenerator3D.cs
+++ Assets/Algorithm/VolMeshGenerator3D.cs
@@ -11,7 +11,7 @@ public class VolMeshGenerator3D : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        generateMesh();
     }
 
     // Update is called once per frame
@@ -32,38 +32,16 @@ public class VolMeshGenerator3D : MonoBehaviour
             mesh.nodes.Add(vertex);
         }
 
-        for (int i = 0; i < triangles.Length; i += 3) {
-            var edgeA = new Edge3D(triangles[i], triangles[i + 2]);
-            var edgeB = new Edge3D(triangles[i + 2], triangles[i + 1]);
-            var edgeC = new Edge3D(triangles[i + 1], triangles[i]);
+        var convexGen = new GK.ConvexHullCalculator();
+        var verts = new List<Vector3>();
+		var tris = new List<int>();
+		var normals = new List<Vector3>();
 
-            int edgeIndexA, edgeIndexB, edgeIndexC;
-
-            if (!mesh.edges.Contains(edgeA)) {
-                mesh.edges.Add(edgeA);
-                edgeIndexA = mesh.edges.Count - 1;
-            } else {
-                edgeIndexA = mesh.edges.FindIndex(edge => edge == edgeA);
-            }
-
-            if (!mesh.edges.Contains(edgeB)) {
-                mesh.edges.Add(edgeB);
-                edgeIndexB = mesh.edges.Count - 1;
-            } else {
-                edgeIndexB = mesh.edges.FindIndex(edge => edge == edgeB);
-            }
-
-            if (!mesh.edges.Contains(edgeC)) {
-                mesh.edges.Add(edgeC);
-                edgeIndexC = mesh.edges.Count - 1;
-            } else {
-                edgeIndexC = mesh.edges.FindIndex(edge => edge == edgeC);
-            }
-
-            // mesh.nodeJointIndexes.Add(new TriangleNodes3D(triangles[i], triangles[i + 2], triangles[i + 1]));
-            // mesh.edgeJointIndexes.Add(new TriangleEdges2D(edgeIndexA, edgeIndexB, edgeIndexC));
-        }
-
+        convexGen.GenerateHull(new List<Vector3>(rawMesh.vertices), ref verts, ref tris, ref normals);
+        rawMesh.Clear();
+        rawMesh.SetVertices(verts);
+        rawMesh.SetTriangles(tris, 0);
+        rawMesh.SetNormals(normals);
         return mesh;
     }
 }
